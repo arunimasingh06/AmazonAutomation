0dfed926cd462bef9294832341e8e1df2c2b9325
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-14 18:14:38 +0530

    Day 12 challenge 2

--- src/main/java/com/amazonautomation/basics/WebTable.java
+++ src/main/java/com/amazonautomation/basics/WebTable.java
@@ -11,9 +11,10 @@ public class WebTable {
         driver.get("https://the-internet.herokuapp.com/tables");
         driver.manage().window().maximize();
         List<WebElement> rows = driver.findElements(
-                By.xpath("//table[@id='table1']//tr")
+                By.xpath("//table[@id='table1']/tbody/tr")
         );
         System.out.println("Number of rows: " + rows.size());
+        double max=0;
         for(WebElement row: rows){
             System.out.println(row.getText());
             List<WebElement> cells = row.findElements(
@@ -23,12 +24,19 @@ public class WebTable {
                 System.out.print(cell.getText()+'|');
             }
             System.out.println();
+            String due= cells.get(3).getText();
+            double amount = Double.parseDouble(
+                    due.replace("$","")
+            );
+            if(amount>max){
+                max=amount;
+            }
         }
         WebElement row= driver.findElement(By.xpath("//table[@id='table1']//tr[td[text()='Smith']]"));
         String email = row.findElement(
                 By.xpath("./td[3]")
         ).getText();
-
+        System.out.println(max);
         System.out.println("Smith's Email: " + email);
     }
 }
