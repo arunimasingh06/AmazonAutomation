c9ac91264936b7c72640602c818c4909f1f6a88f
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-17 02:13:31 +0530

    Day 14 Part 2

---
+++ src/main/java/com/amazonautomation/basics/Exceptions.java
@@ -0,0 +1,28 @@
+package com.amazonautomation.basics;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Exceptions {
+    public static void main (String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.id("start")).click();
+        WebElement hello = driver.findElement(By.id("finish"));
+        WebDriverWait wait =
+                new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement hello = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.id("finish")
+                )
+        );
+        System.out.print(hello.getText());
+
+    }
+}
