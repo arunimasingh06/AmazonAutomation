6be7ad933be43f954ee8364db8a56d2ad1ec13e3
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-19 19:21:57 +0530

    Day 16 part 2

--- src/main/java/com/amazonautomation/pages/LoginPage.java
+++ src/main/java/com/amazonautomation/pages/LoginPage.java
@@ -2,25 +2,35 @@ package com.amazonautomation.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class LoginPage {
     WebDriver driver;
 
-    By username = By.id("username");
-    By password = By.id("password");
-    By loginButton = By.xpath("//button[@type='submit']");
+    @FindBy(id = "username")
+    WebElement username;
+
+    @FindBy(id = "password")
+    WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement loginButton;
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
     public void enterUsername(String user) {
-        driver.findElement(username).sendKeys(user);
+        username.sendKeys(user);
     }
 
     public void enterPassword(String pass) {
-        driver.findElement(password).sendKeys(pass);
+        password.sendKeys(pass);
     }
 
     public void clickLogin() {
-        driver.findElement(loginButton).click();
+        loginButton.click();
     }
 }
