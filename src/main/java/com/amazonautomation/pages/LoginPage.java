94419b3c7b2a2130428650b5f38897c12bd7ff89
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-19 19:53:25 +0530

    Day 16 part 4

--- src/main/java/com/amazonautomation/pages/LoginPage.java
+++ src/main/java/com/amazonautomation/pages/LoginPage.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 
 public class LoginPage {
@@ -18,19 +22,39 @@ public class LoginPage {
 
     @FindBy(xpath = "//button[@type='submit']")
     WebElement loginButton;
+
+    @FindBy(id = "flash")
+    WebElement errorMessage;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
     public void enterUsername(String user) {
+        username.clear();
         username.sendKeys(user);
+        System.out.println("Username value: " + username.getAttribute("value"));
     }
 
     public void enterPassword(String pass) {
+        password.clear();
         password.sendKeys(pass);
+        System.out.println("Password value: " + password.getAttribute("value"));
     }
 
     public void clickLogin() {
+        System.out.println("Clicking login button...");
         loginButton.click();
+    }
+
+    public String getErrorMessage() {
+        WebDriverWait wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(10)
+        );
+
+        return wait.until(
+                ExpectedConditions.visibilityOf(errorMessage)
+        ).getText();
     }
 }
--- src/test/java/com/amazonautomation/tests/LoginTest.java
+++ src/test/java/com/amazonautomation/tests/LoginTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -12,39 +14,50 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class LoginTest {
+    WebDriver driver;
+    LoginPage loginPage;
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(
+                "https://the-internet.herokuapp.com/login"
+        );
+
+        loginPage = new LoginPage(driver);
+    }
 
     @Test
     public void validLogin() {
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--disable-notifications");
-
-        options.setExperimentalOption("prefs", java.util.Map.of(
-                "credentials_enable_service", false,
-                "profile.password_manager_leak_detection", false
-        ));
-        WebDriver driver = new ChromeDriver();
-
-        driver.get("https://the-internet.herokuapp.com/login");
-
-        LoginPage loginPage = new LoginPage(driver);
 
         loginPage.enterUsername("tomsmith");
         loginPage.enterPassword("SuperSecretPassword!");
         loginPage.clickLogin();
-
-        WebDriverWait wait = new WebDriverWait(
-                driver,
-                Duration.ofSeconds(10)
-        );
-
-        wait.until(ExpectedConditions.urlContains("/secure"));
-
+        System.out.println("URL after login: " + driver.getCurrentUrl());
         Assert.assertEquals(
                 driver.getCurrentUrl(),
                 "https://the-internet.herokuapp.com/secure"
         );
+    }
+    @Test
+    public void invalidLogin() {
 
+        loginPage.enterUsername("tomsmith");
+        loginPage.enterPassword("wrongPassword");
+        loginPage.clickLogin();
+
+        String actualMessage =
+                loginPage.getErrorMessage();
+
+        Assert.assertTrue(
+                actualMessage.contains(
+                        "Your password is invalid!"
+                )
+        );
+    }
+
+    @AfterMethod
+    public void tearDown(){
         driver.quit();
     }
 }
