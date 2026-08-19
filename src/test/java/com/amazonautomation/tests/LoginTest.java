43888945b4a6ed68af28d1d63577ca29f1935719
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-19 19:34:27 +0530

    Day 16 part 3

--- src/test/java/com/amazonautomation/tests/LoginTest.java
+++ src/test/java/com/amazonautomation/tests/LoginTest.java
@@ -3,13 +3,26 @@ package com.amazonautomation.tests;
 import com.amazonautomation.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class LoginTest {
 
     @Test
     public void validLogin() {
+        ChromeOptions options = new ChromeOptions();
 
+        options.addArguments("--disable-notifications");
+
+        options.setExperimentalOption("prefs", java.util.Map.of(
+                "credentials_enable_service", false,
+                "profile.password_manager_leak_detection", false
+        ));
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://the-internet.herokuapp.com/login");
@@ -19,6 +32,18 @@ public class LoginTest {
         loginPage.enterUsername("tomsmith");
         loginPage.enterPassword("SuperSecretPassword!");
         loginPage.clickLogin();
+
+        WebDriverWait wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(10)
+        );
+
+        wait.until(ExpectedConditions.urlContains("/secure"));
+
+        Assert.assertEquals(
+                driver.getCurrentUrl(),
+                "https://the-internet.herokuapp.com/secure"
+        );
 
         driver.quit();
     }
