383865ae63aa75edaf7cbb92efa41144705c77c9
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-20 23:07:09 +0530

    Day 18 Part 3

--- src/main/java/com/amazonautomation/data/ExcelReader.java
+++ src/main/java/com/amazonautomation/data/ExcelReader.java
@@ -10,5 +10,12 @@ public class ExcelReader {
                 new FileInputStream("src/main/resources/testdata/LoginData.xlsx");        Workbook workbook = WorkbookFactory.create(file);
         Sheet sheet = workbook.getSheet("LoginData");
         System.out.println(sheet.getSheetName());
+        System.out.println("Number of rows:"+ sheet.getPhysicalNumberOfRows());
+        Row row= sheet.getRow(1);
+        Cell cell = row.getCell(0);
+        System.out.println(cell.getStringCellValue());
+
+        System.out.println(row.getCell(0).getStringCellValue());
+        System.out.println(row.getCell(1).getStringCellValue());
     }
 }
