f99906938f6c2c9ed27cee2068b344e5f7b5442b
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-20 23:16:10 +0530

    Day 18 Part 5

--- src/main/java/com/amazonautomation/data/ExcelReader.java
+++ src/main/java/com/amazonautomation/data/ExcelReader.java
@@ -17,10 +17,42 @@ public class ExcelReader {
 
             String username = row.getCell(0).getStringCellValue();
             String password = row.getCell(1).getStringCellValue();
-
+            boolean expectedSuccess = row.getCell(2).getBooleanCellValue();
+            String expectedMessage = row.getCell(3).getStringCellValue();
             System.out.println("Username: " + username);
             System.out.println("Password: " + password);
+            System.out.println("Expected Success: " + expectedSuccess);
+            System.out.println("Expected Message: " + expectedMessage);
             System.out.println("----------------------");
         }
+        Object[][] data = getExcelData();
+
+        for (Object[] row : data) {
+            System.out.println(
+                    row[0] + " | " +
+                            row[1] + " | " +
+                            row[2] + " | " +
+                            row[3]
+            );
+        }
+    }
+    public static Object[][] getExcelData() throws  IOException{
+        FileInputStream file = new FileInputStream("src/main/resources/testdata/LoginData.xlsx");
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet sheet = workbook.getSheet("LoginData");
+        int rows = sheet.getPhysicalNumberOfRows();
+        Object[][] data = new Object[rows - 1][4];
+        for (int i = 1; i < rows; i++) {
+            Row row = sheet.getRow(i);
+            data[i - 1][0] =
+                    row.getCell(0).getStringCellValue();
+            data[i - 1][1] =
+                    row.getCell(1).getStringCellValue();
+            data[i - 1][2] =
+                    row.getCell(2).getBooleanCellValue();
+            data[i - 1][3] =
+                    row.getCell(3).getStringCellValue();
+        }
+        return data;
     }
 }
