20527d5aa572e4cfb0552989259cbd61c5ae1582
Arunima Singh <arunimasinghhjp12@gmail.com>
2026-08-23 00:47:55 +0530

    Day 20 part 2

---
+++ src/test/java/com/amazonautomation/utils/ConfigReader.java
@@ -0,0 +1,47 @@
+package com.amazonautomation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties;
+
+    static {
+        try {
+            FileInputStream file =
+                    new FileInputStream(
+                            "src/test/resources/config.properties"
+                    );
+
+            properties = new Properties();
+
+            properties.load(file);
+
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+    public static void main(String[] args) {
+
+        System.out.println(
+                ConfigReader.getProperty("browser")
+        );
+
+        System.out.println(
+                ConfigReader.getProperty("url")
+        );
+
+        System.out.println(
+                ConfigReader.getProperty("timeout")
+        );
+    }
+
+}
